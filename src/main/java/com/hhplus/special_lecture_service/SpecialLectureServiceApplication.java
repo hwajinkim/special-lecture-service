72ce663789eab98a0fa9b5eec8a140d7afa3852f
hwa.j <hwajin0720@naver.com>
2024-12-23 01:59:28 +0900

    Init: 프로젝트 초기 세팅

---
+++ src/main/java/com/hhplus/special_lecture_service/SpecialLectureServiceApplication.java
@@ -0,0 +1,13 @@
+package com.hhplus.special_lecture_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpecialLectureServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpecialLectureServiceApplication.class, args);
+	}
+
+}
