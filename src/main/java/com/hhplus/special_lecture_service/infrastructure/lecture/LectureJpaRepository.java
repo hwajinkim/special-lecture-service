1c68b309bc01e6221cb78395fbf921e7a1581e90
hwa.j <hwajin0720@naver.com>
2024-12-29 20:48:24 +0900

    Fix: 특강 신청 가능 목록 조회 JPA 쿼리 수정

--- src/main/java/com/hhplus/special_lecture_service/infrastructure/lecture/LectureJpaRepository.java
+++ src/main/java/com/hhplus/special_lecture_service/infrastructure/lecture/LectureJpaRepository.java
@@ -4,13 +4,15 @@ import com.hhplus.special_lecture_service.domain.lecture.Lecture;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Repository
 public interface LectureJpaRepository extends JpaRepository<Lecture, Long> {
     /*@Query("SELECT l FROM Lecture l WHERE l.lectureDate =:date AND l.isAvailable = 'Y'")*/
-    @Query("SELECT l FROM Lecture l WHERE l.lectureDate = :date AND l.isAvailable = 'Y'")
+    @Query("SELECT l FROM Lecture l WHERE FUNCTION('DATE', l.lectureDate) = :date AND l.isAvailable = 'Y'")
     List<Lecture> findApplicableLectures(@Param("date") String date);
 }
